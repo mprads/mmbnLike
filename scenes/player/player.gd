6cb14d119840970a058f44aa6f6283ffdfb68b18
Myles Rademaker <mylesprademaker@gmail.com>
2024-01-11 18:27:39 -0800

    Moved shared unit logic into a unit class

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,51 +1,17 @@
-extends CharacterBody2D
+extends Unit
 class_name Player
 
-@export var stats: CharacterStats : set = set_character_stats
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var stats_ui: StatsUI = $StatsUI
-@onready var hand: Hand = $Hand
+func _die() -> void:
+	Events.player_died.emit()
 
 
-func set_character_stats(value: CharacterStats) -> void:
-	stats = value
+func get_target_from_raycast() -> Unit:
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(500, 0))
+	var result = space_state.intersect_ray(query)
 
-	#Ensures the player node is ready so there is no blinking of the UI
-	if not stats.stats_changed.is_connected(update_stats):
-		stats.stats_changed.connect(update_stats)
-
-	update_player()
-
-func update_player() -> void:
-	if not stats is CharacterStats:
-		return
-	if not is_inside_tree():
-		await ready
-
-	sprite_2d.texture = stats.art
-	update_stats()
-
-
-func update_stats():
-	stats_ui.update_stats(stats)
-
-func take_damage(damage: int) -> void:
-	if stats.health <= 0:
-		return
-
-	stats.take_damage(damage)
-
-	if stats.health <= 0:
-		queue_free()
-
-
-func move(pos: Vector2) -> void:
-	global_position = pos
-
-
-func _ready() -> void:
-	if not stats_ui.is_node_ready():
-		await stats_ui.ready
-
-	stats_ui.set_max_value(stats)
+	if !result.is_empty():
+		return result["collider"]
+	else:
+		return null
--- scenes/player/player_handler.gd
+++ scenes/player/player_handler.gd
@@ -4,11 +4,18 @@ class_name PlayerHandler
 
 func start_battle(char_stats: CharacterStats) -> void:
 	_create_character(char_stats)
-	new_character.add_to_group("player")
 	draw_cards(character.cards_per_draw)
 
 	#TODO: replace with spawn point
 	new_character.global_position = tile_map.convert_id_to_world_position(Vector2i(1,1))
+
+
+func _set_character(new_character: Unit) -> void:
+	new_character.set_script(load("res://scenes/player/player.gd"))
+	new_character.stats = character
+	new_character.add_to_group("player")
+	add_child(new_character)
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left"):
@@ -20,11 +27,13 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_pressed("down"):
 		_handle_move(Vector2.DOWN)
 
+	#The coupling is tight here with passing a reference to the character to
+	#get access to the tree and the generate raycast function
 	if event.is_action_pressed("use_slot_1"):
-		new_character.hand.get_child(0).play(get_tree())
+		new_character.hand.get_child(0).play(new_character)
 	elif event.is_action_pressed("use_slot_2"):
-		new_character.hand.get_child(1).play(get_tree())
+		new_character.hand.get_child(1).play(new_character)
 	elif event.is_action_pressed("use_slot_3"):
-		new_character.hand.get_child(2).play(get_tree())
+		new_character.hand.get_child(2).play(new_character)
 	elif event.is_action_pressed("use_slot_4"):
-		new_character.hand.get_child(3).play(get_tree())
+		new_character.hand.get_child(3).play(new_character)
